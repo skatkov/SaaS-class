eb45e9ba642c54e66b3c0d6b49632ecb50735a0b
skatkov <lunaticman@gmail.com>
2012-02-23 01:15:27 +0200

    failing unit test for part 1b

--- homework1/part1.rb
+++ homework1/part1.rb
@@ -4,20 +4,36 @@
 require 'test/unit'
 
 class TC_MyTest < Test::Unit::TestCase
+  
+
   def test_panama_true
     assert palindrome?("A man, a plan, a canal -- Panama")
   end
 
   def test_madam_true
-  	assert palindrome?("Madam, I'm Adam!")
+    assert palindrome?("Madam, I'm Adam!")
   end
 
   def test_abracadabra_false
-  	assert_equal false, palindrome?("Abracadabra") 
+    assert_equal false, palindrome?("Abracadabra") 
   end
+
+  def test_count_panama
+    assert_equal ['a' => 3, 'man' => 1, 'canal' => 1, 'panama' => 1, 'plan' => 1], count_words("A man, a plan, a canal -- Panama")
+  end
+
+  def test_count_
+    assert_equal ['doo' => 3, 'bee' => 2], count_words("Doo bee doo bee doo") 
+  end 
 end
 
+#actual implementation
+
 def palindrome?(string)
-	str = string.downcase.gsub(/(\s|\W)/, "")
-  	return str.reverse.eql?(str) 
+  str = string.downcase.gsub(/(\s|\W)/, "")
+    return str.reverse.eql?(str) 
+end
+
+def count_words(string)
+  puts "str:" + string.downcase.gsub(/\b/, "")
 end
