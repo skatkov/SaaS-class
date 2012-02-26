74d1cb0b99c0d7c3e76b856b4d271fa2bd045b83
skatkov <lunaticman@gmail.com>
2012-02-26 18:19:12 +0200

    body for homework part 3

---
+++ homework1/part3.rb
@@ -0,0 +1,28 @@
+# input: ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams',
+'scream']
+# => output: [["cars", "racs", "scar"], ["four"], ["for"],
+["potatoes"], ["creams", "scream"]]
+# HINT: you can quickly tell if two words are anagrams by sorting their
+# letters, keeping in mind that upper vs lowercase doesn't matter
+def combine_anagrams(words)
+#
+<YOUR CODE HERE>
+end
+# input: ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams','scream']
+# => output: [["cars", "racs", "scar"], ["four"], ["for"],["potatoes"], ["creams", "scream"]]
+# HINT: you can quickly tell if two words are anagrams by sorting their
+# letters, keeping in mind that upper vs lowercase doesn't matter
+
+require 'test/unit'
+
+class TC_MyTest < Test::Unit::TestCase
+  def test_anagrams
+    assert_equal [["cars", "racs", "scar"], ["four"], ["for"],["potatoes"], ["creams", "scream"]], 
+    combine_anagrams(['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams','scream'])
+  end
+
+end
+
+def combine_anagrams(words)
+#<YOUR CODE HERE>
+end
