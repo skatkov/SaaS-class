d8ecb33852773386884cab83fd2cbf8c64f010a5
skatkov <lunaticman@gmail.com>
2012-02-27 23:31:43 +0200

    completed part 4 of hw1

--- homework1/part4.rb
+++ homework1/part4.rb
@@ -1,19 +1,63 @@
+require 'test/unit'
+
+class TestBasicOOP < Test::Unit::TestCase
+    def test_dessert_cream
+        ice_cream = Dessert.new 'Ice Cream', 350
+        assert_not_nil ice_cream
+        assert_equal 'Ice Cream', ice_cream.name
+        assert_equal 350, ice_cream.calories
+        assert_equal false, ice_cream.healthy?
+        assert ice_cream.delicious?
+    end
+
+    def test_dessert_ice
+    	ice = Dessert.new "Ice", 199
+        ice.name = 'Ice'
+        ice.calories = 199
+        assert_equal 'Ice', ice.name
+        assert_equal 199, ice.calories
+        assert_equal true, ice.healthy?
+        assert ice.delicious?    	
+    end
+
+    def test_jellyBean
+    	jelly = JellyBean.new "RubbishJelly", 300, "candy"
+    	assert_equal "candy", jelly.flavor 
+    	assert_equal nil, jelly.healthy?
+    	assert jelly.delicious?
+    end
+    'black licorice'
+    def test_jellyBean
+    	jelly = JellyBean.new "RubbishJelly", 300, 'black licorice'
+    	assert_equal false, jelly.delicious? 
+    end
+
+end
+
 class Dessert
+	attr_accessor :name
+	attr_accessor :calories
+
 	def initialize(name, calories)
-	# YOUR CODE HERE
+		@name = name
+		@calories = calories	
 	end
 	def healthy?
-	# YOUR CODE HERE
+		@calories > 200 ? false : true
 	end
 	def delicious?
-	# YOUR CODE HERE
+		true
 	end
 end
+
 class JellyBean < Dessert
+	attr_accessor :flavor
 	def initialize(name, calories, flavor)
-	# YOUR CODE HERE
+		@flavor = flavor
+		@name = name
+		@calories = calories
 	end
 	def delicious?
-	# YOUR CODE HERE
+		@flavor.eql?('black licorice')? false : true
 	end
 end
