f62c03403637f9363f5b7743015976c7f6e08d96
skatkov <lunaticman@gmail.com>
2012-03-01 09:37:50 +0200

    hw1: part 5 of homework

--- homework1/part5.rb
+++ homework1/part5.rb
@@ -1,22 +1,20 @@
-require 'test/unit'
-
-class TC_MyTest < Test::Unit::TestCase
-	def test_bar_history
-		f     = Foo.new
-		f.bar = 1
-		f.bar = 2
-		#assert_equal [nil,1,2], f.bar_history
-	end
-
-end
-
-
 class Class
   def attr_accessor_with_history(attr_name)
 	attr_name = attr_name.to_s   # make sure it's a string
 	attr_reader attr_name        # create the attribute's getter
 	attr_reader attr_name+"_history" # create bar_history getter
-	class_eval "your code here, use %Q for multiline strings"
+	class_eval do
+            
+      define_method(attr_name + "=" ) do |x|
+        @history = Array.new().push(nil) if !defined? @history
+        @history.push(x)
+      end
+      
+      define_method(attr_name + "_history") do
+        return @history
+      end
+      
+    end
   end
 end
 
@@ -24,7 +22,25 @@ class Foo
   attr_accessor_with_history :bar
 end
 
-f     = Foo.new
-f.bar = 1
-f.bar = 2
-f.bar_history # => if your code works, should be [nil,1,2]
+require 'test/unit'
+
+class TC_MyTest < Test::Unit::TestCase
+	
+	def test_bar_history
+		f     = Foo.new
+		f.bar = 1
+		f.bar = 2
+		assert_equal [nil,1,2], f.bar_history
+	end
+
+	def test_bar_history_new
+		f     = Foo.new
+		f.bar = 1
+		f.bar = 2
+		f.bar = Foo.new
+		f.bar = 3
+		assert_equal [nil,3], f.bar_history
+	end
+
+
+end
