8c5f4f794376d644b86c31c7c0e7bb80c483fa91
Stanislav Katkov <stanislav.katkov@ixonos.com>
2012-03-02 11:49:50 +0200

    part5: fixed incorrect unit test

--- homework1/part5.rb
+++ homework1/part5.rb
@@ -1,26 +1,27 @@
 class Class
-  def attr_accessor_with_history(attr_name)
+	def attr_accessor_with_history(attr_name)
 	attr_name = attr_name.to_s   # make sure it's a string
 	attr_reader attr_name        # create the attribute's getter
 	attr_reader attr_name+"_history" # create bar_history getter
 	class_eval %Q{
 		define_method(attr_name + "=" ) do |x|
-        @history = Array.new().push(nil) if !defined? @history
-        @history.push(x)
-      end
-      
-      define_method(attr_name + "_history") do
-        return @history
-      end    
+			if !defined?(@history) then
+				@history = Array.new().push(nil).push(x)
+			else
+				@history.push(x)
+			end
+		end
 
+		define_method(attr_name + "_history") do
+			return @history
+		end    
 	}
-            
-      
-  end
+end
 end
 
 class Foo
-  attr_accessor_with_history :bar
+	attr_accessor_with_history :bar
+	attr_accessor_with_history :second
 end
 
 require 'test/unit'
@@ -34,13 +35,23 @@ class TC_MyTest < Test::Unit::TestCase
 		assert_equal [nil,1,2], f.bar_history
 	end
 
-	def test_bar_history_new
+	def test_bar_history_forget_before_value
 		f     = Foo.new
 		f.bar = 1
 		f.bar = 2
-		f.bar = Foo.new
+		f = Foo.new
 		f.bar = 3
 		assert_equal [nil,3], f.bar_history
+	end
+
+	def test_bar_history_with_multiple
+		f = Foo.new
+		f.bar = 1
+		f.second = 3
+		f.bar = 4
+
+		assert_equal [nil, 1, 4], f.bar_history
+		assert_equal [nil, 3], f.second_history
 	end
 
 
