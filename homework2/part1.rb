650b126cfae727e1e09cc35af7d3b8a3228d331e
Stanislav Katkov <stanislav.katkov@ixonos.com>
2012-03-05 14:47:54 +0200

    part c completed

--- homework2/part1.rb
+++ homework2/part1.rb
@@ -16,11 +16,25 @@ class TC_MyTest < Test::Unit::TestCase
 	def test_rupees_in_euro
 		assert_equal 1, 10.rupees.in(:euro)
 	end
+
+	def test_enum_palindrome_true
+		assert_equal true, [1,2,3,2,1].palindrome?
+	end
+
+	def test_enum_palindrome_false
+		assert_equal false, [1,2,3,2,5].palindrome?
+	end
 end
 
 class String
 	def palindrome?
 		str = self.downcase.gsub(/(\s|\W)/, "")
 		str.reverse.eql?(str) 
+	end
+end
+
+module Enumerable
+	def palindrome?
+		self.to_s.palindrome?		
 	end
 end
