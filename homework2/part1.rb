99b04eaa6e5a6e7baf3db95e613e3a7610371d3f
Stanislav Katkov <stanislav.katkov@ixonos.com>
2012-03-05 14:04:46 +0200

    b of part1 is finished

--- homework2/part1.rb
+++ homework2/part1.rb
@@ -8,4 +8,19 @@ class TC_MyTest < Test::Unit::TestCase
 	def test_palindrome_true
 		assert "Madam in Eden, I'm Adam".palindrome?
 	end
+
+	def test_dollar_to_rupees
+		assert_equal 1, 1.dollar.in(:rupees)
+	end
+
+	def test_rupees_in_euro
+		assert_equal 1, 10.rupees.in(:euro)
+	end
+end
+
+class String
+	def palindrome?
+		str = self.downcase.gsub(/(\s|\W)/, "")
+		str.reverse.eql?(str) 
+	end
 end
