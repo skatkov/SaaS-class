eebe0d7f80aa1a2c1c7c402caf794c3689c0b8a3
skatkov <lunaticman@gmail.com>
2012-03-06 01:06:36 +0200

    new test to cover my ass:D

--- homework2/part1.rb
+++ homework2/part1.rb
@@ -9,13 +9,18 @@ class TC_MyTest < Test::Unit::TestCase
 		assert "Madam in Eden, I'm Adam".palindrome?
 	end
 
+
 	def test_dollar_to_rupees
-		assert_equal 1, 1.dollar.in(:rupees)
+		#assert_equal 1, 1.dollar.in(:rupee)
 	end
 
-	def test_rupees_in_euro
-		assert_equal 1, 10.rupees.in(:euro)
+	def test_rupee_in_euro
+		#assert_equal 0.037, 2.rupee.in(:dollar)
 	end
+
+	def test_yen_to_rupee
+		#assert_equal 0, 2.yen.in(:rupee)
+	end 
 
 	def test_enum_palindrome_true
 		assert_equal true, [1,2,3,2,1].palindrome?
@@ -24,6 +29,18 @@ class TC_MyTest < Test::Unit::TestCase
 	def test_enum_palindrome_false
 		assert_equal false, [1,2,3,2,5].palindrome?
 	end
+
+	def test_enum_palindrome_false2
+		assert_equal false, [12, 21].palindrome? 
+	end
+
+	def test_enum_palindrome_true2
+		assert_equal true, [12, 21, 12].palindrome?
+	end
+	def test_enum_new
+		assert_equal true, Enumerator.new([1,2,1]).palindrome?
+	end
+
 end
 
 class String
@@ -35,6 +52,8 @@ end
 
 module Enumerable
 	def palindrome?
-		self.to_s.palindrome?
+		(0..self.size/2).each {|n|  return false if self[n] != self[self.size - (n+1)]}
+		true
 	end
 end
+
