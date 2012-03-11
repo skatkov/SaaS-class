f0a0a9b31193113efdc3d0e1a970461d78138390
skatkov <lunaticman@gmail.com>
2012-03-11 20:36:24 +0200

    part 2 completed

--- homework2/part2.rb
+++ homework2/part2.rb
@@ -1,16 +1,12 @@
 class CartesianProduct
   include Enumerable
-  # your code here
+
+  def each
+  	@arr1.each{ |a| @arr2.each { |b| yield a , b }}
+  end
+
+  def initialize(a, b)
+  	@arr1 = a
+  	@arr2 = b
+  end 
 end
-
-c = CartesianProduct.new([:a,:b], [4,5])
-c.each { |elt| puts elt.inspect }
-# [:a, 4]
-# [:a, 5]
-# [:b, 4]
-# [:b, 5]
-
-c = CartesianProduct.new([:a,:b], [])
-c.each { |elt| puts elt.inspect }
-# (nothing printed since Cartesian product
-# of anything with an empty collection is empty)
